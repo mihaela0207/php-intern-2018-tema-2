9fc2fd9f43d3b3ee02915ca35389a1825bb5168e
mihaela0207 <mihaela.iordache97@gmail.com>
2018-08-01 10:24:39 +0300

    tema 2 incompleta

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.mainlayout')
+
+
+
+@section('content')
+
+&nbsp;&nbsp;&nbsp;<div class="album text-muted">
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div class="container">
+
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div class="row">
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<h1>This is a demo text</h1>
+
+
+
+</div>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+
+&nbsp;&nbsp;&nbsp;</div>
+
+@endsection
+
+
+
+
+
+
+
+
+<b></b>
---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,13 @@
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.1/examples/album/album.css" rel="stylesheet">
